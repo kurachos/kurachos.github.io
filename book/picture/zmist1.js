ae936ad18ee26b8c9e91a190dd6e8bf8258354d6
kurachos <47923926+kurachos@users.noreply.github.com>
2019-04-05 17:11:31 +0300

    Add files via upload

---
+++ book/picture/zmist1.js
@@ -0,0 +1,24 @@
+	 
+	  var elemIDsecond_blok = document.getElementById('second_blok');
+	  	 
+	         elemIDsecond_blok.style.fontFamily = 'Arial'; 
+	 	   elemIDsecond_blok.style.color == 'black';
+	  function change() {
+		  if ( elemIDsecond_blok.style.color == 'black') {
+	  
+	  	  elemIDsecond_blok.style.fontFamily = 'Georgia'; 
+	      
+	  	  elemIDsecond_blok.style.fontSize = '16px';
+	  	  elemIDsecond_blok.style.color = 'green';
+	  
+       } else 
+		   
+		   {
+			 elemIDsecond_blok.style.fontFamily = 'verdana'; 
+	     
+	  	  elemIDsecond_blok.style.fontSize = '12px';
+	  	  elemIDsecond_blok.style.color = 'black';
+		   }
+	  }
+ 
+	
